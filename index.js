070ecba2a698d8b5620a0d31913c3f1523cfca07
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2016-03-06 16:41:12 -0300

    fixed minor bug

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const githubNpmProfile = require('github-npm-profile');
 
 module.exports = (githubUsername, options) => {
   return githubNpmProfile(githubUsername, options)
-    .then(npmUser)
     .then(profile => profile.twitter);
 };
