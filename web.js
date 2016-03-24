3e65a143e3aecd9e51bf171e509f6dcb22177275
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2016-03-24 18:28:42 -0300

    logging

--- web.js
+++ web.js
@@ -9,4 +9,7 @@ app.get('/:username', cors(), (req, res) => {
   getUsername(req.params.username, { gh })
     .then(username => res.end(username));
 });
-app.listen(port);
+app.listen(port, () => console.info('Listening on port %s', port));
+
+process.on('uncaughtException', err => console.error('Uncaught Exception\n%s', err.stack || err));
+process.on('unhandledRejection', err => console.error('Unhandled Rejection\n%s', err.stack || err));
